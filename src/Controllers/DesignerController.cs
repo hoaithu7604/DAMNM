3145e81492f970a93ea55079d8ed76fe3cf3caad
Thu Nguyen <hoaithu7604@gmail.com>
2019-11-30 15:57:45 +0700

    add workflowengine

---
+++ src/Controllers/DesignerController.cs
@@ -0,0 +1,69 @@
+﻿using OptimaJet.Workflow;
+using OptimaJet.Workflow.Core.Builder;
+using OptimaJet.Workflow.Core.Bus;
+using OptimaJet.Workflow.Core.Runtime;
+using OptimaJet.Workflow.Core.Parser;
+using System.Collections.Generic;
+using System;
+using System.Collections.Specialized;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using System.Xml.Linq;
+using WorkflowRuntime = OptimaJet.Workflow.Core.Runtime.WorkflowRuntime;
+
+namespace WF.Sample.Controllers
+{
+    public class DesignerController : Controller
+    {
+        public ActionResult Index(string schemeName)
+        {
+            return View();
+        }
+        
+        public ActionResult API()
+        {
+            Stream filestream = null;
+            if (Request.Files.Count > 0)
+                filestream = Request.Files[0].InputStream;
+
+            var pars = new NameValueCollection();
+            pars.Add(Request.Params);
+            
+            if(Request.HttpMethod.Equals("POST", StringComparison.InvariantCultureIgnoreCase))
+            {
+                var parsKeys = pars.AllKeys;
+                foreach (var key in Request.Form.AllKeys)
+                {
+                    if (!parsKeys.Contains(key))
+                    {
+                        pars.Add(Request.Form);
+                    }
+                }
+            }
+
+            var res = getRuntime.DesignerAPI(pars, filestream, true);
+            var operation = pars["operation"].ToLower();
+            if (operation == "downloadscheme")
+                return File(Encoding.UTF8.GetBytes(res), "text/xml", "scheme.xml");
+	        else if (operation == "downloadschemebpmn")
+                return File(UTF8Encoding.UTF8.GetBytes(res), "text/xml", "scheme.bpmn");
+
+            return Content(res);
+        }
+
+      private WorkflowRuntime getRuntime
+        {
+            get
+            {
+                //INIT YOUR RUNTIME HERE
+                return WorkflowEngine.WorkflowInit.Runtime;
+            }
+        }
+    }
+}
+
+
+ 
